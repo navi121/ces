0008041dcfdc718f333382857d6bc6568a728ef6
navi121 <37406888+navi121@users.noreply.github.com>
2021-07-07 17:54:55 +0530

    Added Loop Program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            if (a > b)
+                Console.WriteLine("a GREATER THAN b");
+            else
+                Console.WriteLine("B greater than A");
+        }
+    }
+}
